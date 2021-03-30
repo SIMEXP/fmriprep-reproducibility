8a0ce70d6806ab5346fecd32fe7e4cffe7f49770
ltetrel <loic.tetrel.pro@gmail.com>
2021-03-30 16:26:03 -0400

    first version of submit scripts

---
+++ code/run.bash
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+PROJECT_DIR=../../fmriprep-lts
+OPENNEURO=$PROJECT_DIR/inputs/openneuro/
+FMRIPREP_CONTAINER=$PROJECT_DIR/envs/fmriprep-20.2.1lts.sif
+# user variables
+TEST=false # whether to print the commands or launch them
+SLURM=false # whether to submit slurm jobs or raw cmd
+ACCOUNT= # slurm account name
+MAIL_USER= # mail for job status
+
+# argument parser
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    --test)
+    TEST=true
+    shift # past argument
+    ;;
+    --slurm)
+    SLURM=true
+    shift # past argument
+    ;;
+    -a|--account)
+    ACCOUNT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -m|--mail)
+    MAIL_USER="$2"
+    shift # past argument
+    shift # past value
+    ;;
+esac
+done
+
+echo "TEST = ${TEST}"
+echo "SLURM = ${SLURM}"
+echo "ACCOUNT = ${ACCOUNT}"
+echo "MAIL_USER = ${MAIL_USER}"
+
+# read all dataset keys
+DATASET_KEYS=($(singularity exec -B $PROJECT_DIR:/WORK $FMRIPREP_CONTAINER \
+  python3 -c """
+import os
+import json
+with open(os.path.join(\"/WORK\", \"code\", \"fmriprep-cmd.json\")) as f: data = json.load(f)
+list_keys = \" \".join(data.keys())
+print(list_keys)
+  """
+  ))
+
+# Loop through all datasets
+for DATASET in ${DATASET_KEYS[@]}
+do
+    # read all participant keys
+    export SINGULARITYENV_DATASET=$DATASET
+	PARTICIPANT_KEYS=($(singularity exec -B $PROJECT_DIR:/WORK $FMRIPREP_CONTAINER \
+      python3 -c """
+import os
+import json
+with open(os.path.join(\"/WORK\", \"code\", \"fmriprep-cmd.json\")) as f: data = json.load(f)
+list_keys = \" \".join(data[os.environ.get(\"DATASET\")].keys())
+print(list_keys)
+      """
+      ))
+
+    # loop through all participants
+    for PARTICIPANT in ${PARTICIPANT_KEYS[@]}
+    do
+        # slurm cmd
+        if [ "$SLURM" = true ] ; then
+            read -r -d '' CMD <<- EOM
+                sbatch
+                --account=$ACCOUNT
+                --job-name=fmriprep_$DATASET-$PARTICIPANT_%A_%a.job
+                --mail-user=$MAIL_USER
+                --output=/scratch/%u/fmriprep_$DATASET-$PARTICIPANT_%A_%a.out
+                --error=/scratch/%u/fmriprep_$DATASET-$PARTICIPANT_%A_%a.err
+                slurm/fmriprep-slurm.bash $DATASET $PARTICIPANT
+EOM
+        # raw cmd
+        else
+            read -r -d '' CMD <<- EOM
+                raw singularity command
+EOM
+        fi
+        # print command
+        if [ "$TEST" = true ] ; then
+            echo $CMD
+        # execute command
+        else
+            $CMD
+        fi
+    done
+done
---
+++ code/slurm/fmriprep-slurm.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#SBATCH --array=1-5
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=4096M
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+DATASET=$1
+PARTICIPANT=$2
+export SINGULARITYENV_FS_LICENSE=$HOME/.freesurfer.txt
+export SINGULARITYENV_TEMPLATEFLOW_HOME=/templateflow
+
+module load singularity/3.6
+
+#copying input dataset into local scratch space
+rsync -rltv --info=progress2 $SCRATCH/fmriprep-lts $SLURM_TMPDIR
+
+singularity run --cleanenv -B $SLURM_TMPDIR/fmriprep-lts:/WORK -B $HOME/.cache/templateflow:/templateflow -B /etc/pki:/etc/pki/ \
+    $SLURM_TMPDIR/fmriprep-lts/envs/fmriprep-20.2.1lts.sif \
+    -w /WORK/fmriprep_work \
+    --output-spaces MNI152NLin2009cAsym MNI152NLin6Asym \
+    --notrack --write-graph --resource-monitor \
+    --omp-nthreads 8 --nprocs 16 --mem_mb 65536 \
+    --random-seed 0 --fs-no-reconall --participant-label $PARTICIPANT \
+    /WORK/inputs/openneuro/$DATASET /WORK/inputs/openneuro/$DATASET/derivatives/fmriprep participant \
+fmriprep_exitcode=$?
+
+if [ $fmriprep_exitcode -ne 0 ] ; then
+    cp -R $SLURM_TMPDIR/fmriprep-lts/fmriprep_work $SCRATCH/fmriprep_$DATASET-$PARTICIPANT_$SLURM_ARRAY_TASK_ID.workdir
+fi 
+if [ $fmriprep_exitcode -eq 0 ] ; then
+    cp -R $SLURM_TMPDIR/fmriprep-lts/inputs/openneuro/$DATASET/derivatives/fmriprep $SCRATCH/fmriprep_$DATASET_$SLURM_ARRAY_TASK_ID
+    cp $SLURM_TMPDIR/fmriprep-lts/fmriprep_work/fmriprep_wf/resource_monitor.json $SCRATCH/fmriprep_$DATASET_$SLURM_ARRAY_TASK_ID/fmriprep_$DATASET-$PARTICIPANT_resource_monitor_$SLURM_ARRAY_TASK_ID.json 
+fi 
+exit $fmriprep_exitcode 
