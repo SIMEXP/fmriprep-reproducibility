2a44f84faceabfcdfb566bc7f516d38c287f1f28
ltetrel <loic.tetrel.pro@gmail.com>
2021-03-30 16:25:21 -0400

    now loops through json files to get participants

--- code/get_data.bash
+++ code/get_data.bash
@@ -4,38 +4,62 @@ PROJECT_DIR=../../fmriprep-lts
 OPENNEURO=$PROJECT_DIR/inputs/openneuro/
 FMRIPREP_CONTAINER=$PROJECT_DIR/envs/fmriprep-20.2.1lts.sif
 
-# Here get the container, more specifically for slurm compatibility
-
+######### Get the raw data
 ## Age
-# children male
-datalad get -r $OPENNEURO/ds000256/sub-CTS201
-# children female
-datalad get -r $OPENNEURO/ds000256/sub-CTS210
-# young male
-datalad get -r $OPENNEURO/ds001748/sub-adult15
-# young female
-datalad get -r $OPENNEURO/ds001748/sub-adult16
-# adult male
-datalad get -r $OPENNEURO/ds002338/sub-xp207
-# adult female
-datalad get -r $OPENNEURO/ds002338/sub-xp201
-# senior male
-datalad get -r $OPENNEURO/ds002080/sub-PAT23
-# senior female
-datalad get -r $OPENNEURO/ds002080/sub-PAT10
-
-# Fieldmaps
+# children male ds000256/sub-CTS201
+# children female ds000256/sub-CTS210
+# young male ds001748/sub-adult15
+# young female ds001748/sub-adult16
+# adult male /ds002338/sub-xp207
+# adult female /ds002338/sub-xp201
+# senior male ds002080/sub-PAT23
+# senior female ds002080/sub-PAT10
+## Fieldmaps
 # Phase-difference map and at least one magnitude image
 # Two phase maps and two magnitude images
 # Multiple phase encoded directions ("pepolar")
-datalad get -r $OPENNEURO/ds001600/sub-1
-
-# non-parametric structural MR images
+# ds001600/sub-1
+## Non-parametric structural MR images
 # T2w
 # SBref
-datalad get -r $OPENNEURO/ds001771/sub-36
+# ds001771/sub-36
 
-# make sure to download all the templateflow templates
+# read all dataset keys
+DATASET_KEYS=($(singularity exec -B $PROJECT_DIR:/WORK $FMRIPREP_CONTAINER \
+  python3 -c """
+import os
+import json
+with open(os.path.join(\"/WORK\", \"code\", \"fmriprep-cmd.json\")) as f: data = json.load(f)
+list_keys = \" \".join(data.keys())
+print(list_keys)
+  """
+  ))
+
+# Loop through all datasets
+for DATASET in ${DATASET_KEYS[@]}
+do
+    # read all participant keys
+    export SINGULARITYENV_DATASET=$DATASET
+	PARTICIPANT_KEYS=($(singularity exec -B $PROJECT_DIR:/WORK $FMRIPREP_CONTAINER \
+      python3 -c """
+import os
+import json
+with open(os.path.join(\"/WORK\", \"code\", \"fmriprep-cmd.json\")) as f: data = json.load(f)
+list_keys = \" \".join(data[os.environ.get(\"DATASET\")].keys())
+print(list_keys)
+      """
+      ))
+    # loop through all participants
+    for PARTICIPANT in ${PARTICIPANT_KEYS[@]}
+    do
+        datalad get -r $OPENNEURO/$DATASET/$PARTICIPANT
+    done
+done
+
+######### Get the container
+
+
+######### Download the templateflow templates
 export SINGULARITYENV_TEMPLATEFLOW_HOME=/templateflow
 singularity exec -B $PROJECT_DIR:/WORK \
   -B /etc/pki:/etc/pki \
@@ -43,5 +67,5 @@ singularity exec -B $PROJECT_DIR:/WORK \
   $FMRIPREP_CONTAINER \
   python3 -c """
 from templateflow.api import get;
-get(['MNI152NLin2009cAsym', 'MNI152NLin6Asym', 'OASIS30ANTs', 'MNIPediatricAsym', 'MNIInfant'])
+get(['MNI152NLin2009cAsym', 'MNI152NLin6Asym'])
 """
