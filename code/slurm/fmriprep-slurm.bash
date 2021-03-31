c53c8184b128157e9062a6850bd0ad4540389a0d
ltetrel <loic.tetrel.pro@gmail.com>
2021-03-31 12:50:51 -0400

    now saving into output dir of input repo; some variables refactoring

--- code/slurm/fmriprep-slurm.bash
+++ code/slurm/fmriprep-slurm.bash
@@ -10,16 +10,19 @@
 
 DATASET=$1
 PARTICIPANT=$2
+INPUT_DIR=$SCRATCH/fmriprep-lts
+OUTPUT_DIR=$INPUT_DIR/outputs/ieee
+SING_IMG=fmriprep-20.2.1lts.sif
 export SINGULARITYENV_FS_LICENSE=$HOME/.freesurfer.txt
 export SINGULARITYENV_TEMPLATEFLOW_HOME=/templateflow
 
 module load singularity/3.6
 
 #copying input dataset into local scratch space
-rsync -rltv --info=progress2 $SCRATCH/fmriprep-lts $SLURM_TMPDIR
+rsync -rltv --info=progress2 $INPUT_DIR $SLURM_TMPDIR
 
 singularity run --cleanenv -B $SLURM_TMPDIR/fmriprep-lts:/WORK -B $HOME/.cache/templateflow:/templateflow -B /etc/pki:/etc/pki/ \
-    $SLURM_TMPDIR/fmriprep-lts/envs/fmriprep-20.2.1lts.sif \
+    $SLURM_TMPDIR/fmriprep-lts/envs/$SING_IMG \
     -w /WORK/fmriprep_work \
     --output-spaces MNI152NLin2009cAsym MNI152NLin6Asym \
     --notrack --write-graph --resource-monitor \
@@ -32,7 +35,7 @@ if [ $fmriprep_exitcode -ne 0 ] ; then
     cp -R $SLURM_TMPDIR/fmriprep-lts/fmriprep_work $SCRATCH/fmriprep_$DATASET-$PARTICIPANT_$SLURM_ARRAY_TASK_ID.workdir
 fi 
 if [ $fmriprep_exitcode -eq 0 ] ; then
-    cp -R $SLURM_TMPDIR/fmriprep-lts/inputs/openneuro/$DATASET/derivatives/fmriprep $SCRATCH/fmriprep_$DATASET_$SLURM_ARRAY_TASK_ID
-    cp $SLURM_TMPDIR/fmriprep-lts/fmriprep_work/fmriprep_wf/resource_monitor.json $SCRATCH/fmriprep_$DATASET_$SLURM_ARRAY_TASK_ID/fmriprep_$DATASET-$PARTICIPANT_resource_monitor_$SLURM_ARRAY_TASK_ID.json 
+    cp -R $SLURM_TMPDIR/fmriprep-lts/inputs/openneuro/$DATASET/derivatives/fmriprep $OUTPUT_DIR/fmriprep_$DATASET_$SLURM_ARRAY_TASK_ID
+    cp $SLURM_TMPDIR/fmriprep-lts/fmriprep_work/fmriprep_wf/resource_monitor.json $OUTPUT_DIR/fmriprep_$DATASET_$SLURM_ARRAY_TASK_ID/fmriprep_$DATASET-$PARTICIPANT_resource_monitor_$SLURM_ARRAY_TASK_ID.json 
 fi 
 exit $fmriprep_exitcode 
