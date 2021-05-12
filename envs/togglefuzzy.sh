ae6b106c006d40f108fb954ff55e0b04c10c71ce
Tristan Glatard <tristan.glatard@concordia.ca>
2021-05-12 15:07:12 -0400

    Fixed entrypoint, started OSF sibling

--- envs/togglefuzzy.sh
+++ envs/togglefuzzy.sh
@@ -4,9 +4,9 @@ set -e
 
 if [ -v FUZZY ] && [ $FUZZY == "true" ]
 then
-    LIB_WRAP='/lib/fuzzy/docker/resources/libmath/libmath.so'
+    LIB_WRAP='/opt/mca-libmath/libmath.so'
     test -f ${LIB_WRAP} || (echo "ERROR: cannot find ${LIB_WRAP}" && false)
-    export LD_PRELOAD="${LIB_WRAP} /usr/lib/x86_64-linux-gnu/libdl.so"
+    export LD_PRELOAD="${LIB_WRAP}"
     echo "Preloaded ${LIB_WRAP}"
 fi
 
