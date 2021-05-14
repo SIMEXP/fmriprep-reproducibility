1184b2d2cee9d3eb063dd31404e6e595cb33fc04
ltetrel <loic.tetrel.pro@gmail.com>
2021-05-14 15:23:42 -0400

    rename containers to docker for better comprehension; re-worked build.sh so it is relative to ./code (YODA)

--- code/docker/build.sh
+++ code/docker/build.sh
@@ -3,11 +3,14 @@
 set -e
 set -u
 
+DOCKERFILE_DIR=$(readlink -e $(dirname $0))
+ENVSPATH=$(realpath ${DOCKERFILE_DIR}/../../envs)
+
 function docker2singularity(){
     local docker_image=$1
     local sing_image_name=$2
 
-    local mounts="-v $PWD/envs:/tmp -v $PWD:$PWD -w $PWD"
+    local mounts="-v ${ENVSPATH}:/tmp -v $PWD:$PWD -w $PWD"
     
     docker run -v /var/run/docker.sock:/var/run/docker.sock ${mounts}\
            quay.io/singularity/docker2singularity -n ${sing_image_name}.sif ${docker_image}
@@ -18,15 +21,13 @@ function docker2singularity(){
     
 }
 
-# Run this script from the base repo
-
 FMRIPREP_VERSION=20.2.0
 
 # Build regular (non-fuzzy) image from Poldrack lab Docker image
-docker2singularity poldracklab/fmriprep:${FMRIPREP_VERSION} fmriprep-lts-${FMRIPREP_VERSION}
+#docker2singularity poldracklab/fmriprep:${FMRIPREP_VERSION} fmriprep-lts-${FMRIPREP_VERSION}
 
 # Build fuzzy image
-docker build . --build-arg fmriprep_version=${FMRIPREP_VERSION} -f code/containers/Dockerfile -t fmriprep-lts-fuzzy:${FMRIPREP_VERSION}
+docker build . --build-arg fmriprep_version=${FMRIPREP_VERSION} -f ${DOCKERFILE_DIR}/Dockerfile -t fmriprep-lts-fuzzy:${FMRIPREP_VERSION}
 
 docker2singularity fmriprep-lts-fuzzy:${FMRIPREP_VERSION} fmriprep-lts-fuzzy-${FMRIPREP_VERSION}
 
